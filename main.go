865f9aa6996ee92aff199b2cef0e0a4fdd28c57a
Carter <speers.carter@gmail.com>
2024-07-07 19:16:24 -0400

    added more styling and integrated HTMX for dynamic updating without reload ;)

--- main.go
+++ main.go
@@ -11,5 +11,8 @@ func main() {
 		port = "8080"
 	}
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/editor", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "./static/editor.html")
+	})
 	http.ListenAndServe(":"+port, nil)
 }
